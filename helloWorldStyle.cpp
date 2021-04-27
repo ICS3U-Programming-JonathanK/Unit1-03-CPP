20730e2fae99305d86cdb699491958418616b6d6
Ubuntu <ubuntu@ip-172-31-18-62.ec2.internal>
2021-04-27 18:24:41 +0000

    added in helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Year Jonathan Kene All rights reserved.
+//
+// Created by: Jonathan Kene
+// Date: April 27, 2021
+// This program displays "Hello, World with style".
+
+
+#include <iostream>
+
+
+int main() {
+std::cout << "Hello, World with style!" << std::endl;
+}
